decd8d1dbab1ada726f314fadec695c864ed202a
why <DESKTOP-EAFBBGF.FX63>
2023-10-15 00:03:47 +0800

    15 - 19

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,8 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a = "123451234512345123412345123451234512341234512345123451234123451234512345123412345123451234512345555512345123451234512345"; 
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
