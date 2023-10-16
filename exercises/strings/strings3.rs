d4009f3fc8be9fad3b6b4112b86188910ee07387
why <DESKTOP-EAFBBGF.FX63>
2023-10-17 00:02:25 +0800

    31 - 39

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,38 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+
+    // let mut start = 0; 
+    // for(_start, c) in input.chars().enumerate() {
+    //     if c != ' ' {
+    //         start = _start;
+    //         break;
+    //     }
+    // }
+
+    // let mut end = 0;
+    // for(_end, c) in input.chars().rev().enumerate() {
+    //     if c != ' ' {
+    //         end = input.len() - _end;
+    //         break;
+    //     }
+    // }
+
+    // input[start..end].to_string()
+
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    String::from(input) + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
